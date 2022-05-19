5802998192511d8c06c9a0e9b2d238a6c3915b45
jiho <wjeeho@gmail.com>
2022-05-19 22:40:36 +0900

    회원인지 확인후에 암호화까지 비교하고 토큰 정보 저장

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const mongoose = require('mongoose');
 const { User } =require('./models/User') ;
 const bodyParser = require('body-parser');
 const config = require('./config/key');
+const cookieParser = require('cookie-parser');
 
 //bodyparser로 분석해서 가져올 수 있게
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+
+//cookie사용
+app.use(cookieParser());
 
 mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true
@@ -27,4 +31,24 @@ app.post('/register', (req,res)=>{
         return res.status(200).json({registerSuccess: true})
     })
 })
+
+app.post('/login', (req,res)=>{
+    User.findOne({ email: req.body.email},(err, userConfirm) => {
+        if(!userConfirm) res.json({loginSuccess : false})
+        
+        userConfirm.comparePassword(req.body.password, (err, pwConfirm)=>{
+            if(!pwConfirm) res.json({ loginSuccess : false, message: "비밀번호 틀림"});
+
+
+            userConfirm.ititToken((err, user)=>{
+                if(err) return res.status(400).send(err);
+                
+                res.cookie("new cookie",user.token).status(200)
+                .json({loginSuccess: true, userId: user._id, message: "다 맞아서 토큰도 생성함"})
+            })
+        })
+    })
+})
+
+
 app.listen(port, ()=>console.log(`Example app listening on port ${port}!`));
--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+var jsonwebtoken = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -27,6 +29,46 @@ const userSchema = mongoose.Schema({
     },
 })
 
+userSchema.pre('save', function (next){
+    var user = this;
+
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err);
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err);
+                user.password = hash;
+                next();
+            });
+        });
+    }else{
+        
+        next();
+    }
+})
+
+userSchema.methods.comparePassword = function(pwConfirm, callback){
+    //확인된 유저의 패스워드
+    bcrypt.compare(pwConfirm, this.password, function(err, isMatch){
+        if(err) return callback(err);
+        callback(null, isMatch);
+    })
+}
+
+userSchema.methods.ititToken = function(callback){
+    var user = this;
+    var token = jsonwebtoken.sign(user._id.toHexString(), 'everything');
+    user.token = token;
+    user.save(function(err, user){
+        if(err) return callback(err);
+        callback(null, user);
+    });
+}
+
+
+
 const User = mongoose.model('User', userSchema);
+
+
 
 module.exports = { User };
